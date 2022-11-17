035a0d90590f1c79334b05e348454efa51a95286
olabk <olabaut@gmail.com>
2022-11-17 18:29:35 +0100

    start project

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+//Przygotuj referencję do szablonu oraz listy .books-list.
+initData: function(){
+    const thisApp = this;
+    thisApp.data= dataSource.books;
+    const books = .books-list
+},
+
+function render(){
+for (const book in dataSource.books) 
+}
+//Wewnątrz niej przejdź po każdym elemencie z dataSource.books. Pamiętaj, że plik script.js ma do tego obiektu bezpośredni dostęp.
+//Wewnątrz tej pętli zadbaj o wygenerowanie kodu HTML na podstawie szablonu oraz danych o konkretnej książce.
+//Na postawie tego kodu HTML wygeneruj element DOM.
+//Wygenerowany element DOM dołącz jako nowe dziecko DOM do listy .books-list.
