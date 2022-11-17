3e1fdcdaf1ed4e5845a33a632ce060b847688817
olabk <olabaut@gmail.com>
2022-11-17 18:52:06 +0100

    zadanie 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 //Przygotuj referencję do szablonu oraz listy .books-list.
-initData: function(){
-    const thisApp = this;
-    thisApp.data= dataSource.books;
-    const books = .books-list
-},
+const bookList = document.querySelector('.books-list');
+const bookTemplateScript = document.querySelector('#template-book');
+const bookTemplate = Handlebars.compile(bookTemplateScript.innerHTML);
 
 function render(){
-for (const book in dataSource.books) 
+  for (const book of dataSource.books) {
+    const generatedHTML = bookTemplate(book);
+    const element = utils.createDOMFromHTML(generatedHTML);
+    bookList.appendChild(element);
+  }
 }
-//Wewnątrz niej przejdź po każdym elemencie z dataSource.books. Pamiętaj, że plik script.js ma do tego obiektu bezpośredni dostęp.
-//Wewnątrz tej pętli zadbaj o wygenerowanie kodu HTML na podstawie szablonu oraz danych o konkretnej książce.
-//Na postawie tego kodu HTML wygeneruj element DOM.
-//Wygenerowany element DOM dołącz jako nowe dziecko DOM do listy .books-list.
+render();
+
